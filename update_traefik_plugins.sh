2f265b436bcca666dda0651abbc0d8a84a7b61b2
Imran <30463115+ImranR98@users.noreply.github.com>
2025-12-13 17:04:46 +0300

    Added a note

--- update_traefik_plugins.sh
+++ update_traefik_plugins.sh
@@ -14,7 +14,7 @@ for l in $(echo "$PLUGIN_LINES" | grep -o '\.plugins\..*\.modulename=[^"]*'); do
 	PLUGIN_LATEST_VERSION="$(curl -s https://api.github.com/repos$(echo $PLUGIN_URL | sed s/github\.com//g'')/releases/latest | grep -oE 'tag/.*' | tail -c +5 | head -c -3)"
 	if [ "$PLUGIN_CURRENT_VERSION" != "$PLUGIN_LATEST_VERSION" ]; then
 		sed -i "s/\.plugins\.$PLUGIN_NAME\.version=$PLUGIN_CURRENT_VERSION/.plugins.$PLUGIN_NAME.version=$PLUGIN_LATEST_VERSION/g" landscape.docker-compose.yaml
-		echo "Plugin $PLUGIN_NAME updated to $PLUGIN_LATEST_VERSION"
+		echo "Plugin $PLUGIN_NAME updated to $PLUGIN_LATEST_VERSION (you need to restart Traefik for this to take effect)"
 	else
 		echo "Plugin $PLUGIN_NAME already on latest ($PLUGIN_LATEST_VERSION)"
 	fi
