030bafb6d4f54edcc06818100ec72c9487467393
Imran <30463115+ImranR98@users.noreply.github.com>
2026-05-15 23:41:08 -0400

    Bugfix

--- compose.sh
+++ compose.sh
@@ -5,7 +5,7 @@ HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 case "${1:-}" in
     luna|lens|sol)
-        TARGET="$1"
+        export TARGET="$1"
         shift
         ;;
     "")
