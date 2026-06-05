de71603c3a61e8e4230f18c1d19228d6ad8c8f68
Imran <30463115+ImranR98@users.noreply.github.com>
2026-06-05 14:05:53 -0400

    Add token-based auth middleware for owncast

--- targets/pc/VARS.template.sh
+++ targets/pc/VARS.template.sh
@@ -5,3 +5,6 @@ export DOMAIN_OWNER_EMAIL="contact@$SERVICES_DOMAIN"
 # --- FRP ---
 export PROXY_HOST="pcproxy.$SERVICES_DOMAIN"
 export FRPC_TOKEN="change_me" # openssl rand -hex 128
+
+# --- Owncast viewer access ---
+export OWNCAST_ACCESS_TOKEN="change_me" # openssl rand -hex 32
