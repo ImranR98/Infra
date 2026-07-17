d8d5a26f6345f972a7e6c62ef7ae0a301d624fc7
Imran <30463115+ImranR98@users.noreply.github.com>
2026-07-17 14:34:14 +0100

    fix: recreate local-tls Certificate in cert-manager component (was in deleted Longhorn)

--- targets/srv0/k3s/cert-manager/post.sh
+++ targets/srv0/k3s/cert-manager/post.sh
@@ -31,3 +31,9 @@ for _ in $(seq 1 30); do
 	kubectl get clusterissuer letsencrypt-staging >/dev/null 2>&1 && break
 	sleep 5
 done
+
+echo "Applying certificates..."
+for _ in $(seq 1 30); do
+	envsubst "$ENVSUBST_VARS" <"$COMP_DIR/certificates.yaml" | kubectl apply -f - 2>/dev/null && break
+	sleep 5
+done
