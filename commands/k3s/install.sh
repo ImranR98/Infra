61b4e82ed2b7e630e5e1c893ab97e8d9b8b100ad
Imran <30463115+ImranR98@users.noreply.github.com>
2026-07-21 10:08:36 +0100

    Simplify awk: only fix lines that lost all indentation (indent=0), rely on VARS for nesting

--- commands/k3s/install.sh
+++ commands/k3s/install.sh
@@ -59,10 +59,9 @@ _build_yaml() {
 			/:[[:blank:]]*\|-?[[:blank:]]*$/ { ki=indent($0); blk=1; base=0; got=0; print; next }
 			blk && !got && NF>0             { base=indent($0); got=1; print; next }
 			blk && NF==0                    { blk=0; print; next }
-			blk && (indent($0)<ki && /:/ || /^---/ || /^\.\.\./) { blk=0; print; next }
-			blk && indent($0)>=base         { print; next }
-			blk { $0=sprintf("%*s%s",base,"",substr($0,indent($0)+1)); print; next }
-			{ print }
+			blk && (/^---/ || /^\.\.\./)    { blk=0; print; next }
+			blk && indent($0)==0 && NF>0    { $0=sprintf("%*s%s",base,"",$0); print; next }
+			{ blk=0; print }
 		')
 }
 
